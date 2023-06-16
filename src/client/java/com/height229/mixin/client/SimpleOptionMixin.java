5a0ba1f2b630255ecbc1aec5396e077e6322738b
height229 <height229@existnetwork.com>
2023-06-17 00:36:06 +0200

    Added project files for 1.20

---
+++ src/client/java/com/height229/mixin/client/SimpleOptionMixin.java
@@ -0,0 +1,24 @@
+package com.height229.mixin.client;
+
+import com.height229.reFullbright;
+import net.minecraft.client.option.SimpleOption;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
+
+
+@Mixin(SimpleOption.DoubleSliderCallbacks.class)
+public class SimpleOptionMixin {
+
+    @Inject(method = "validate(Ljava/lang/Double;)Ljava/util/Optional;", at = @At("RETURN"), cancellable = true)
+    public void removeValidation(Double double_, CallbackInfoReturnable<Optional<Double>> cir) {
+        if(reFullbright.enabled) {
+            if(double_ == 1500.0) {
+                cir.setReturnValue(Optional.of(1500.0));
+            }
+        }
+    }
+}
